f04672a181ef06573d196d8321507bd0f6989bce
HACHILES, MATHEW EDGIE H. <mathewedgieh@gmail.com>
2025-03-11 10:56:41 +0800

    initial commit

---
+++ ageCheck.js
@@ -0,0 +1,13 @@
+/*If Statement Activity*/
+let age = prompt("Enter your age:");
+age = Number(age);
+
+if(age < 13){
+    console.log("You are a child.");
+}else if (age >= 13 && age <= 19){
+    console.log("You are a teenager.");
+}else if (age >= 20){
+    console.log("You are an adult.");
+}else {
+    console.log("Invalid age entered.");
+}
